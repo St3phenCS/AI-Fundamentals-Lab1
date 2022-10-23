d5cc7d6fe32c05c822dbb3b5305b2315049e693e
20120540 <st3phencs@gmail.com>
2022-10-23 18:31:17 +0700

    fix bug visualize

--- advanceAlg.py
+++ advanceAlg.py
@@ -27,7 +27,7 @@ def ALG(MAZE,heuristic,bonus):
     dx=[-1, 0, 1, 0, -1]
     cntNode=0
     cost =-1
-    VIS=[]
+    vis=[]
     importantPoints=[]
     for row in range(len(MAZE)):
         for col in range(len(MAZE[row])):
@@ -44,7 +44,7 @@ def ALG(MAZE,heuristic,bonus):
         currPoint=node.coord
         w=node.w
         cntNode+=1
-        VIS.append(currPoint)
+        vis.append(currPoint)
         if currPoint == goal:
             importantPoints.append(goal)
             src=importantPoints.pop(0)
@@ -56,7 +56,7 @@ def ALG(MAZE,heuristic,bonus):
            
             solution.insert(0,start)   
             cost+=len(solution)   
-            return start,goal,VIS,solution,cntNode,cost       
+            return start,goal,vis,solution,cntNode,cost       
         if currPoint in bonus:
                 cost+=bonus[currPoint]
                 cntNode+=bonus[currPoint]
